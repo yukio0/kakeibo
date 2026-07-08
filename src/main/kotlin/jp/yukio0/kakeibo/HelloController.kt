ab71afab7360e049b703b5cd33b700672bb18f7c
yukio0 <suugakun@gmail.com>
2026-07-08 21:02:16 +0900

    hello

---
+++ src/main/kotlin/jp/yukio0/kakeibo/HelloController.kt
@@ -0,0 +1,9 @@
+package jp.yukio0.kakeibo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+  @GetMapping("/hello") fun hello(): String = "Hello World!"
+}
