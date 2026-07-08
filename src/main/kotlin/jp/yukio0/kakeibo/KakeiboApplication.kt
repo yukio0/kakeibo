8b8fcccfe36fceac22cfc4edf65dfcbfb5380c13
yukio0 <suugakun@gmail.com>
2026-07-08 20:52:41 +0900

    spring boot initializr

---
+++ src/main/kotlin/jp/yukio0/kakeibo/KakeiboApplication.kt
@@ -0,0 +1,11 @@
+package jp.yukio0.kakeibo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KakeiboApplication
+
+fun main(args: Array<String>) {
+	runApplication<KakeiboApplication>(*args)
+}
