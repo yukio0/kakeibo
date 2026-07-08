452ea85d93e61a46a10ef89caa40501c56c47cf5
yukio0 <suugakun@gmail.com>
2026-07-08 20:55:22 +0900

    spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,55 @@
 plugins {
-	kotlin("jvm") version "2.3.21"
-	kotlin("plugin.spring") version "2.3.21"
-	id("org.springframework.boot") version "4.1.0"
-	id("io.spring.dependency-management") version "1.1.7"
+  kotlin("jvm") version "2.3.21"
+  kotlin("plugin.spring") version "2.3.21"
+  id("org.springframework.boot") version "4.1.0"
+  id("io.spring.dependency-management") version "1.1.7"
+  id("com.diffplug.spotless") version "8.8.0"
 }
 
 group = "jp.yukio0"
+
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
 }
 
 repositories {
-	mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  implementation("org.springframework.boot:spring-boot-starter")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
-	}
+  compilerOptions {
+    freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+  }
+}
+
+spotless {
+  kotlin {
+    target("src/**/*.kt")
+    ktfmt().googleStyle()
+    trimTrailingWhitespace()
+    endWithNewline()
+  }
+
+  kotlinGradle {
+    target("*.gradle.kts", "gradle/**/*.gradle.kts")
+    ktfmt().googleStyle()
+    trimTrailingWhitespace()
+    endWithNewline()
+  }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+  useJUnitPlatform()
 }
--- src/main/kotlin/jp/yukio0/kakeibo/KakeiboApplication.kt
+++ src/main/kotlin/jp/yukio0/kakeibo/KakeiboApplication.kt
@@ -3,9 +3,8 @@ package jp.yukio0.kakeibo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class KakeiboApplication
+@SpringBootApplication class KakeiboApplication
 
 fun main(args: Array<String>) {
-	runApplication<KakeiboApplication>(*args)
+  runApplication<KakeiboApplication>(*args)
 }
